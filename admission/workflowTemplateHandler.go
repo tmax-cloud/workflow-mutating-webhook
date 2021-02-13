31f496da46b47cdb5fddb2bfa18ce6f8e88fa288
RhoJeongwan <jeongwan_rho@tmax.co.kr>
2021-02-13 15:17:05 +0900

    [feat] workflow template 관련코드 추가

--- admission/workflowTemplateHandler.go
+++ admission/workflowTemplateHandler.go
@@ -1,12 +1,46 @@
 package admission
 
 import (
+	"encoding/json"
 	"fmt"
 
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"k8s.io/api/admission/v1beta1"
+	"k8s.io/klog"
 )
 
 func WorkflowTemplateSACheck(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
-	fmt.Println("TODO for WorkflowTemplate")
-	return nil
+	reviewResponse := v1beta1.AdmissionResponse{}
+
+	fmt.Println("check for enter metadata handler")
+
+	ms := wfv1.WorkflowTemplate{}
+	ds := "default-editor"
+
+	if err := json.Unmarshal(ar.Request.Object.Raw, &ms); err != nil {
+		return ToAdmissionResponse(err) //msg: error
+	}
+
+	//수정사항을 담을 구조체 slice
+	var patch []patchOps
+
+	if len(ms.Spec.ServiceAccountName) == 0 {
+		createPatch(&patch, "add", "/spec/serviceAccountName", ds)
+	}
+	klog.Infof("check data for ms : %s", ms.Spec)
+
+	// 구조체 slice에 저장된 patch를 []byte로 변경
+	if patchData, err := json.Marshal(patch); err != nil {
+		return ToAdmissionResponse(err) //msg: error
+	} else {
+		klog.Infof("JsonPatch=%s", string(patchData))
+		reviewResponse.Patch = patchData
+	}
+
+	// v1beta1 pkg에 저장된 patchType (const string)을 Resp에 저장
+	pt := v1beta1.PatchTypeJSONPatch
+	reviewResponse.PatchType = &pt
+	reviewResponse.Allowed = true
+
+	return &reviewResponse
 }
