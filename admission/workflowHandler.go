d9cc5eae43d2d2c19110c83e48ae73d09f3a75d6
yuhanjung <74162105+yuhanjung@users.noreply.github.com>
2021-03-17 13:58:12 +0900

    Workflow SA check and add default-editor

--- admission/workflowHandler.go
+++ admission/workflowHandler.go
@@ -3,11 +3,16 @@ package admission
 import (
 	"encoding/json"
 	"fmt"
-
+	
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"k8s.io/api/admission/v1beta1"
 	"k8s.io/klog"
+	//"context"
+	//sav1 "k8s.io/client-go/1.5/kubernetes/typed/core/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+
 
 func WorkflowSACheck(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	reviewResponse := v1beta1.AdmissionResponse{}
@@ -21,14 +26,42 @@ func WorkflowSACheck(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 		return ToAdmissionResponse(err) //msg: error
 	}
 
+	/* serviceaccount client-go
+	ctx := context.Background()
+	sa := sav1.serviceAccounts{}
+	//clicmd.GetDefaultServer()
+	getopt := metav1.GetOptions{}
+	sa.Get(ctx, "default-editor", getopt)*/
+
+
+
 	//수정사항을 담을 구조체 slice
 	var patch []patchOps
 
-	if len(ms.Spec.ServiceAccountName) == 0 && len(ms.Spec.WorkflowTemplateRef.Name) == 0 {
-		createPatch(&patch, "add", "/spec/serviceAccountName", ds)
+	a := 0
+	if len(ms.Spec.ServiceAccountName) == 0 && ms.Spec.WorkflowTemplateRef == nil {
+		klog.Infof("in if")
+		if len(ms.Spec.Templates) == 0 { 
+			klog.Infof("in if, len templates 0")
+			createPatch(&patch, "add", "/spec/serviceAccountName", ds)
+		} else {
+			klog.Infof("in if, len templates != 0")
+			for i := 0 ; i < len(ms.Spec.Templates); i++ {
+				if len(ms.Spec.Templates[i].ServiceAccountName) == 0 {
+					a = a+1
+					//templates의 항목에 넣어주는 부분
+					//templatestring := "/spec/templates[" + a + "]/serviceAccountName"
+					//klog.Infof("check data for templatestring : %s", templatestring)
+					//createPatch(&patch, "add", templatestring, ds)
+				} 
+			}
+			if a > 0{
+				createPatch(&patch, "add", "/spec/serviceAccountName", ds)
+			}
+		} 
 	}
-	klog.Infof("check data for ms : %s", ms.Spec)
-
+	//klog.Infof("check data for ms.Spec : %s", ms.Spec)
+	
 	// 구조체 slice에 저장된 patch를 []byte로 변경
 	if patchData, err := json.Marshal(patch); err != nil {
 		return ToAdmissionResponse(err) //msg: error
--- go.mod
+++ go.mod
@@ -3,27 +3,36 @@ module hypercloud4-webhook
 go 1.14
 
 require (
+	github.com/Azure/go-autorest/autorest v0.11.1 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.5 // indirect
 	github.com/argoproj/argo v0.0.0-20210104234559-93ee530126cc
 	github.com/coyove/jsonbuilder v0.0.0-20160414062945-90ee6d2c3c43
-	github.com/evanphx/json-patch v4.5.0+incompatible
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/evanphx/json-patch v4.9.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/websocket v1.4.2
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/juju/ratelimit v1.0.1 // indirect
 	github.com/lib/pq v1.8.0
+	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/yuhanjung/argo v1.0.3
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	google.golang.org/genproto v0.0.0-20201204160425-06b3db808446 // indirect
 	google.golang.org/grpc v1.33.1
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/api v0.18.8
-	k8s.io/apimachinery v0.18.8
+	k8s.io/api v0.20.4
+	k8s.io/apimachinery v0.20.4
 	k8s.io/apiserver v0.18.8
-	k8s.io/client-go v0.18.8
+	
 	k8s.io/klog v1.0.0
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
 )
+
+
