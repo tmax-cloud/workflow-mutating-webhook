8fae0e9e431a1a02e5cd53014244f1c7b0f08d2b
yuhanjung <74162105+yuhanjung@users.noreply.github.com>
2021-04-21 13:14:09 +0900

    create serviceaccount
    success to create service account

--- admission/workflowHandler.go
+++ admission/workflowHandler.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-
+	"runtime/debug"
+	
 	wfv1 "github.com/argoproj/argo/v2/pkg/apis/workflow/v1alpha1"
 	//wfv1 "github.com/argoproj/argo-workflows/pkg/apis/workflow/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
 	"k8s.io/client-go/kubernetes"
@@ -32,15 +34,49 @@ func WorkflowSACheck(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 		panic(err.Error())
 	}
 
+	// argo namespace에서 default-editor 확인
 	_, err = clientset.CoreV1().ServiceAccounts("argo").Get(context.TODO(), ds, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		fmt.Printf("default-editor isn't exist\n")
+		//없을 경우 여기에서 SA 생성코드 작성
+		
+		config2, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err)
+		}
+		client, err := kubernetes.NewForConfig(config2)
+		if err != nil {
+			panic(err)
+		}
+		serviceAccountClient := client.CoreV1().ServiceAccounts("argo")
+
+		serviceAccount := &corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+					Name: "default-editor",
+			},	
+		}
+		result, err := serviceAccountClient.Create(context.TODO(), serviceAccount, metav1.CreateOptions{})
+		
+		if err != nil {
+			defer func() {
+				if r := recover(); r != nil { 
+					fmt.Println("Recovered", r) 
+					debug.PrintStack() 
+				}
+			}()
+			panic(err)
+		}
+		klog.Infof("Created service account %v.", result.GetObjectMeta().GetName())
+		
 
 	} else if err != nil {
 		panic(err.Error())
 	} else {
 		fmt.Printf("default-editor is exist\n")
 	}
+
+	//service account에 rbac 적용 필요
+
 
 
 	ms := wfv1.Workflow{}
